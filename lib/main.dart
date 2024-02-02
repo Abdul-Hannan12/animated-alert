946d80d982226c4b8d11f0f09be2be4e8a3e0130
Abdul Hannan <abdulhannan8540680@gmail.com>
2024-02-02 11:56:21 +0500

    made alert buttons

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Alerts',
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animated Alerts"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.check_circle_outline_rounded,
+                size: 35,
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                fixedSize: Size(MediaQuery.sizeOf(context).width * 0.8, 80),
+              ),
+              label: const Text(
+                'Success',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.cancel_outlined,
+                size: 35,
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                fixedSize: Size(MediaQuery.sizeOf(context).width * 0.8, 80),
+              ),
+              label: const Text(
+                'Error     ',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
